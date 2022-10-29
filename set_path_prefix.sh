0727cc44cb55ca9a0de60b3f087acc8169d84380
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-10-28 20:03:41 -0400

    test: see if BASEURL is playing the real part

--- set_path_prefix.sh
+++ set_path_prefix.sh
@@ -7,7 +7,5 @@ if [[ "$branch" = "main" ]]; then
 fi
 
 if [ -z $LOCAL ]; then
-  echo "Setting prefix path"
-  sed -i "s#pathPrefix = '/';#pathPrefix = '/preview/gsa/datagov-11ty/$branch';#g" .eleventy.js
-  grep "pathPrefix = '" .eleventy.js
+  export BASEURL=/preview/gsa/datagov-11ty/$branch
 fi
