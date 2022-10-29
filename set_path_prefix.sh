044ee84f1ee6e6f23655e9ce6bc14d0a516ff430
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-10-28 20:16:21 -0400

    test: non-bash script

--- set_path_prefix.sh
+++ set_path_prefix.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
-
 branch=$(git rev-parse --abbrev-ref HEAD)
-
-if [[ "$branch" = "main" ]]; then
-  exit 0;
-fi
 
 if [ -z $LOCAL ]; then
   export BASEURL=/preview/gsa/datagov-11ty/$branch
+fi
+
+if [ "$branch" = "main" ]; then
+  echo "Everything is correct.  Leaving configuration alone"
+  export BASEURL=/
 fi
