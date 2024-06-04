cba1ca5d647a4e7eb441716dfc3960f6c394206f
robert-bryson <robert.bryson@gsa.gov>
2024-06-04 09:21:26 -0700

    wip

--- js/fetch.js
+++ js/fetch.js
@@ -10,111 +10,130 @@ const mapDataGovColors = () => DATAGOV_COLOR_PALETTE.map((color) => `rgba(${colo
 const deNormalizeMetrics = (number) => Math.floor(Math.pow(10, number));
 
 const metricConfigs = {
-    getData: (el) => JSON.parse(decodeURIComponent(el.dataset.metric)),
-    buildPieConfig: (el) => {
-        const data = metricConfigs.getData(el);
-        const config = {
-            type: 'doughnut',
-            data: {
-                labels: data.map((row) => row.label),
-                datasets: [
-                    {
-                        data: data.map((row) => row.count),
-                        backgroundColor: mapDataGovColors(),
-                    },
-                ],
+  getData: (el) => JSON.parse(decodeURIComponent(el.dataset.metric)),
+  buildPieConfig: (el) => {
+    const data = metricConfigs.getData(el);
+    const config = {
+      type: 'doughnut',
+      data: {
+        labels: data.map((row) => row.label),
+        datasets: [
+          {
+            data: data.map((row) => row.count),
+            backgroundColor: mapDataGovColors(),
+          },
+        ],
+      },
+      options: {
+        plugins: {
+          title: {
+            display: true,
+            text: 'Dataset Distribution',
+          },
+        },
+      },
+    };
+    return config;
+  },
+  buildDatasetBarConfig: (el) => {
+    const data = metricConfigs.getData(el);
+    const config = {
+      type: 'bar',
+      data: {
+        labels: ['Older', 'Last year', 'Last month', 'Last week'],
+        datasets: data.map((row) => ({
+          data: row.data,
+          backgroundColor: mapDataGovColors(),
+        })),
+      },
+      options: {
+        aspectRatio: window.innerWidth > 480 && window.innerWidth < 1024 ? 1 : 2,
+        plugins: {
+          legend: {
+            display: false,
+          },
+          title: {
+            display: true,
+            text: 'Dataset Age',
+          },
+        },
+      },
+    };
+    return config;
+  },
+  buildOrgBarConfig: (el) => {
+    const data = metricConfigs.getData(el);
+    const config = {
+      type: 'bar',
+      data: {
+        labels: ['Agencies', 'Datasets', 'Harvest Sources'],
+        datasets: data.map((row, index) => ({
+          label: row.label,
+          data: row.data,
+          backgroundColor: `rgba(${DATAGOV_COLOR_PALETTE[index]}, ${DATAGOV_COLOR_OPACITY})`,
+        })),
+      },
+      options: {
+        aspectRatio: window.innerWidth < 480 ? 0.75 : 1.405,
+        plugins: {
+          legend: {
+            display: window.innerWidth < 480 ? false : true,
+          },
+          tooltip: {
+            callbacks: {
+              label: function (context) {
+                let label = context.dataset.label || '';
+                let trueValue = deNormalizeMetrics(context.formattedValue) || '';
+                return `${label}: ${trueValue}`;
+              },
             },
-            options: {
-                plugins: {
-                    title: {
-                        display: true,
-                        text: 'Dataset Distribution',
-                    },
-                },
+          },
+          title: {
+            display: true,
+            text: 'Key Metrics by Organization Type',
+          },
+        },
+        scales: {
+          y: {
+            ticks: {
+              callback: function (value, index, ticks) {
+                return deNormalizeMetrics(value);
+              },
             },
-        };
-        return config;
-    },
-    buildDatasetBarConfig: (el) => {
-        const data = metricConfigs.getData(el);
-        const config = {
-            type: 'bar',
-            data: {
-                labels: ['Older', 'Last year', 'Last month', 'Last week'],
-                datasets: data.map((row) => ({
-                    data: row.data,
-                    backgroundColor: mapDataGovColors(),
-                })),
-            },
-            options: {
-                aspectRatio: window.innerWidth > 480 && window.innerWidth < 1024 ? 1 : 2,
-                plugins: {
-                    legend: {
-                        display: false,
-                    },
-                    title: {
-                        display: true,
-                        text: 'Dataset Age',
-                    },
-                },
-            },
-        };
-        return config;
-    },
-    buildOrgBarConfig: (el) => {
-        const data = metricConfigs.getData(el);
-        const config = {
-            type: 'bar',
-            data: {
-                labels: ['Agencies', 'Datasets', 'Harvest Sources'],
-                datasets: data.map((row, index) => ({
-                    label: row.label,
-                    data: row.data,
-                    backgroundColor: `rgba(${DATAGOV_COLOR_PALETTE[index]}, ${DATAGOV_COLOR_OPACITY})`,
-                })),
-            },
-            options: {
-                aspectRatio: window.innerWidth < 480 ? 0.75 : 1.405,
-                plugins: {
-                    legend: {
-                        display: window.innerWidth < 480 ? false : true,
-                    },
-                    tooltip: {
-                        callbacks: {
-                            label: function (context) {
-                                let label = context.dataset.label || '';
-                                let trueValue = deNormalizeMetrics(context.formattedValue) || '';
-                                return `${label}: ${trueValue}`;
-                            },
-                        },
-                    },
-                    title: {
-                        display: true,
-                        text: 'Key Metrics by Organization Type',
-                    },
-                },
-                scales: {
-                    y: {
-                        ticks: {
-                            callback: function (value, index, ticks) {
-                                return deNormalizeMetrics(value);
-                            },
-                        },
-                    },
-                },
-            },
-        };
-        return config;
-    },
+          },
+        },
+      },
+    };
+    return config;
+  },
+  buildMetricsPagesLast7DaysConfig: (el) => {
+    const data = metricConfigs.getData(el);
+    const config = {
+      type: 'bar',
+      data: {
+        labels: data.map((row) => row.label),
+        datasets: [
+          {
+            data: data.map((row) => row.count),
+            backgroundColor: mapDataGovColors(),
+          },
+        ],
+      },
+    };
+    return config;
+  },
 };
 
 (async function () {
-    const piEl = document.getElementById('datagov-pie-chart');
-    new Chart(piEl, metricConfigs.buildPieConfig(piEl));
+  const piEl = document.getElementById('datagov-pie-chart');
+  new Chart(piEl, metricConfigs.buildPieConfig(piEl));
 
-    const orgBarEl = document.getElementById('datagov-bar-chart-org');
-    new Chart(orgBarEl, metricConfigs.buildOrgBarConfig(orgBarEl));
+  const orgBarEl = document.getElementById('datagov-bar-chart-org');
+  new Chart(orgBarEl, metricConfigs.buildOrgBarConfig(orgBarEl));
 
-    const datasetBarEl = document.getElementById('datagov-bar-chart-datasets');
-    new Chart(datasetBarEl, metricConfigs.buildDatasetBarConfig(datasetBarEl));
+  const datasetBarEl = document.getElementById('datagov-bar-chart-datasets');
+  new Chart(datasetBarEl, metricConfigs.buildDatasetBarConfig(datasetBarEl));
+
+  const requestPagesLast7 = document.getElementById('metricsRequestsLast7Days');
+  new Chart(requestPagesLast7, metricConfigs.buildDatasetBarConfig(requestPagesLast7));
 })();
---
+++ pages/metrics.md
@@ -0,0 +1,20 @@
+---
+date: '2009-10-01T17:15:15'
+excerpt: 'Data.gov Metrics Dashboard'
+link: https://www.data.gov/metrics
+permalink: 'metrics/'
+redirect_from:
+    - /dashboard/
+
+slug: metrics
+title: Datagov Metrics
+---
+
+<div id="metrics">
+  <div style="width: 800px;">
+    <canvas id="metricsRequestsLast7Days"></canvas>
+    <canvas id="metricsRequestsLast28Days"></canvas>
+  </div>"
+</div>
+
+<!-- <script type="text/javascript" src="../_data/metrics.js"></script> -->
