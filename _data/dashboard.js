ad6cfae9c277dc7ba5af31c305abecd6f91dbfe5
Tyler Burton <tyler.burton@gsa.gov>
2024-07-17 13:16:19 -0500

    feedback fixes and cleanup

--- _data/dashboard.js
+++ _data/dashboard.js
@@ -6,35 +6,45 @@ const CACHE_CONFIG = {
   type: 'json',
 };
 
-const buildDevicePieMetric = (reportPath) => {
-    const data = require(reportPath);
-    const pie = [];
-    for (let key in data.labels) {
-      pie.push({
-        label: data.labels[key],
-        count: data.data[key],
-      });
-    }
-    return encodeURIComponent(JSON.stringify(pie));
-  };
-  
-  const buildDescBarChartMetric = (reportPath) => {
-    const data = require(reportPath);
-    const bar = [];
-    for (let key in data.labels) {
-      bar.push({
-        label: data.labels[key],
-        count: data.data[key],
-      });
-    }
-    return encodeURIComponent(JSON.stringify(bar));
-  }
-
-
-module.exports = async function () {  
+async function downloadObject(url) {
   try {
+    const fetchResponse = await fetch(url);
+    return await fetchResponse.json();
+  } catch (err) {
+    console.error('Error - ', err);
+  }
+}
+
+const buildDevicePieMetric = (reportPath) => {
+  const data = require(reportPath);
+  const pie = [];
+  for (let key in data.labels) {
+    pie.push({
+      label: data.labels[key],
+      count: data.data[key],
+    });
+  }
+  return encodeURIComponent(JSON.stringify(pie));
+};
+
+const buildDescBarChartMetric = (data) => {
+  const bar = [];
+  for (let key in data.labels) {
+    bar.push({
+      label: data.labels[key],
+      count: data.data[key],
+    });
+  }
+  return encodeURIComponent(JSON.stringify(bar));
+}
+
+
+module.exports = async function () {
+  try {
+    let data = await downloadObject('https://s3-us-gov-west-1.amazonaws.com/cg-6348e2ae-48ea-4133-ac22-8f248c43e1fd/top_search_terms_last30.json')
+    console.log(data)
     const metrics = {
-      topSearchTermsMetric: buildDescBarChartMetric(path.resolve('./reports/top_search_terms_last30.json'))
+      topSearchTermsMetric: buildDescBarChartMetric(data)
     };
 
     return {
