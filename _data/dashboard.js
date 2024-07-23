7c66b331d026d1c113ee24a163ad06f668afc8e6
Tyler Burton <tyler.burton@gsa.gov>
2024-07-23 17:55:59 -0500

    adds report construct mapper pattern and org metrics pages

--- _data/dashboard.js
+++ _data/dashboard.js
@@ -1,31 +1,89 @@
 require('dotenv').config();
+const EleventyFetch = require('@11ty/eleventy-fetch');
 const path = require("path");
+const csv = require("csvtojson/v2");
+const request = require('request')
 
-const CACHE_CONFIG = {
-  duration: '1d',
-  type: 'json',
-};
+const S3_BASE_URL = 'https://s3-us-gov-west-1.amazonaws.com/cg-baa85e06-1bdd-4672-9e3a-36333c05c6ce/'
 
-async function downloadObject(url) {
-  try {
-    const fetchResponse = await fetch(url);
-    return await fetchResponse.json();
-  } catch (err) {
-    console.error('Error - ', err);
+const GET_ORG_LIST_URL = "https://catalog.data.gov/api/action/organization_list?all_fields=true&limit=1"
+
+// number of reports to display when data should be truncated.
+const TRUNCATED_COUNT = 10
+
+const REPORTS = {
+  GLOBAL: {
+    PAGEVIEWS: {
+      title: "",
+      description: "",
+      url: "global__total_pageviews__last30.csv"
+    },
+    DEVICE_TYPE: {
+      title: "",
+      description: "",
+      url: "global__device_category__last30.csv"
+    },
+    DATASETS_PER_ORG: {
+      title: "",
+      description: "",
+      url: "global__datasets_per_org.csv"
+    },
+    HARVEST_SORUCES_PER_ORG: {
+      title: "",
+      description: "",
+      url: "global__harvest_sources.csv"
+    },
+    TOP_SEARCH_TERMS: {
+      title: "",
+      description: "",
+      url: "global__top_search_terms__last30.csv"
+    }
+  },
+  // ORG is a meta constructor for orgs
+  ORG: {
+    MOST_VIEWED_DATASETS: {
+      title: "Most Viewed Dataset Pages",
+      description: "Top 10 dataset pages by pageviews in the last 30 days",
+      url: "link_requests__last30.csv"
+    },
+    MOST_DOWNLOADED_DATASETS: {
+      title: "Most Downloaded Datasets",
+      description: "Top 10 files by download count in the last 30 days",
+      url: "download_requests__last30.csv"
+    },
+    MOST_CLICKED_OUTBOUND_LINKS: {
+      title: "Most Clicked Outbound Links",
+      description: "Top 10 external link clicks in the last 30 days",
+      url: "link_requests__last30.csv"
+    }
   }
 }
 
-const buildDevicePieMetric = (reportPath) => {
-  const data = require(reportPath);
-  const pie = [];
-  for (let key in data.labels) {
-    pie.push({
-      label: data.labels[key],
-      count: data.data[key],
-    });
-  }
-  return encodeURIComponent(JSON.stringify(pie));
-};
+CACHE_DURATION = '22h'
+
+async function downloadCSVFromS3(url) {
+  const csvString = await EleventyFetch(url, {
+    duration: CACHE_DURATION,
+    method: 'get',
+    "headers": {
+      "content-type": 'text/csv;charset=UTF-8'
+    }
+  })
+    .then((response) => response.toString())
+
+  return csv({
+    noheader: true,
+    output: "csv"
+  }).fromString(csvString)
+    .then((csvRow) => csvRow)
+}
+
+const downloadJSON = async (url) => {
+  return await EleventyFetch(url, {
+    duration: CACHE_DURATION,
+    type: 'json'
+  });
+}
 
 const buildDescBarChartMetric = (data) => {
   const bar = [];
@@ -41,14 +99,69 @@ const buildDescBarChartMetric = (data) => {
 
 module.exports = async function () {
   try {
-    let data = await downloadObject('https://s3-us-gov-west-1.amazonaws.com/cg-6348e2ae-48ea-4133-ac22-8f248c43e1fd/top_search_terms_last30.json')
+
+    // get org infos
+    let { result } = await downloadJSON(GET_ORG_LIST_URL);
+    let orgList = result;
+
+    // get data
+    const data = {}
+    for (let type in REPORTS) {
+      if (type == "ORG") {
+        // add org forloop
+        for (let org of orgList) {
+          let orgName = org.name
+          data[orgName] = {}
+          for (let report in REPORTS[type]) {
+            data[orgName][report] = await downloadCSVFromS3(`${S3_BASE_URL}${orgName}__${REPORTS.ORG[report].url}`)
+          }
+        }
+      } else {
+        data[type] = {}
+        for (let report in REPORTS[type]) {
+          data[type][report] = await downloadCSVFromS3(`${S3_BASE_URL}${REPORTS[type][report].url}`)
+        }
+
+      }
+    }
+
+    // now structure org data
+    const orgData = {}
+    for (let org of orgList) {
+      let orgName = org.name
+      orgData[orgName] = []
+      for (let report in REPORTS.ORG) {
+        report = {
+          meta: {
+            key: report,
+            title: REPORTS.ORG[report].title,
+            description: REPORTS.ORG[report].description,
+            reportLink: `${S3_BASE_URL}${orgName}__${REPORTS.ORG[report].url}`
+          },
+          // truncate data before attaching
+          data: data[orgName][report].slice(0, TRUNCATED_COUNT)
+        }
+        orgData[orgName].push(report)
+      }
+
+
+    }
+    // build charts
     const metrics = {
-      topSearchTermsMetric: buildDescBarChartMetric(data)
+      topSearchTermsMetric: buildDescBarChartMetric(data.GLOBAL.TOP_SEARCH_TERMS)
     };
 
+    // save to friendly namespace
+    const global = data.GLOBAL
+    const orgs = orgData
+
     return {
+      global,
+      orgs,
       metrics,
+      orgList
     };
+
   } catch (e) {
     if (process.env.NODE_ENV === 'prod') {
       // Fail the build in production.
