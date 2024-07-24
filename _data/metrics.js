2010584084b2900b49058d0749b764970695135a
Tyler Burton <tyler.burton@gsa.gov>
2024-07-24 10:53:56 -0500

    adds new global page; removes static data

--- _data/metrics.js
+++ _data/metrics.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const EleventyFetch = require('@11ty/eleventy-fetch');
-const path = require("path");
 const csv = require("csvtojson/v2");
-const request = require('request')
 
 const S3_BASE_URL = 'https://s3-us-gov-west-1.amazonaws.com/cg-baa85e06-1bdd-4672-9e3a-36333c05c6ce/'
 
@@ -14,28 +12,28 @@ const TRUNCATED_COUNT = 10
 const REPORTS = {
   GLOBAL: {
     PAGEVIEWS: {
-      title: "",
-      description: "",
+      title: "Total Pageviews",
+      description: "Top pageviews sitewide",
       url: "global__total_pageviews__last30.csv"
     },
     DEVICE_TYPE: {
-      title: "",
+      title: "Pageviews by Device Type",
       description: "",
       url: "global__device_category__last30.csv"
     },
     DATASETS_PER_ORG: {
-      title: "",
-      description: "",
+      title: "Number of Datasets per Organization",
+      description: "Count of datasets by organization",
       url: "global__datasets_per_org.csv"
     },
-    HARVEST_SORUCES_PER_ORG: {
-      title: "",
-      description: "",
+    HARVEST_SOURCES_PER_ORG: {
+      title: "Number of Harvest Sources per Organization",
+      description: "Count of harvest sources by organization",
       url: "global__harvest_sources.csv"
     },
     TOP_SEARCH_TERMS: {
-      title: "",
-      description: "",
+      title: "Top Search Terms",
+      description: "Top searchterms sitewide",
       url: "global__top_search_terms__last30.csv"
     }
   },
@@ -44,7 +42,7 @@ const REPORTS = {
     MOST_VIEWED_DATASETS: {
       title: "Most Viewed Dataset Pages",
       description: "Top 10 dataset pages by pageviews in the last 30 days",
-      url: "link_requests__last30.csv"
+      url: "page_requests__last30.csv"
     },
     MOST_DOWNLOADED_DATASETS: {
       title: "Most Downloaded Datasets",
@@ -125,35 +123,56 @@ module.exports = async function () {
       }
     }
 
-    // now structure org data
-    const orgData = {}
-    for (let org of orgList) {
-      let orgName = org.name
-      orgData[orgName] = []
-      for (let report in REPORTS.ORG) {
-        report = {
-          meta: {
-            key: report,
-            title: REPORTS.ORG[report].title,
-            description: REPORTS.ORG[report].description,
-            reportLink: `${S3_BASE_URL}${orgName}__${REPORTS.ORG[report].url}`
-          },
-          // truncate data before attaching
-          data: data[orgName][report].slice(0, TRUNCATED_COUNT)
+    // now structure data
+    const shapedData = {}
+    for (let type in REPORTS) {
+      if (type == "ORG") {
+        shapedData[type] = {}
+        for (let org of orgList) {
+          let orgName = org.name
+          shapedData[type][orgName] = []
+          for (let report in REPORTS.ORG) {
+            report = {
+              meta: {
+                key: report,
+                title: REPORTS.ORG[report].title,
+                description: REPORTS.ORG[report].description,
+                reportLink: `${S3_BASE_URL}${orgName}__${REPORTS.ORG[report].url}`
+              },
+              // truncate data before attaching
+              data: data[orgName][report].slice(0, TRUNCATED_COUNT)
+            }
+            shapedData[type][orgName].push(report)
+          }
+
+
         }
-        orgData[orgName].push(report)
+      } else {
+        shapedData[type] = []
+        for (let report in REPORTS[type]) {
+          report = {
+            meta: {
+              key: report,
+              title: REPORTS[type][report].title,
+              description: REPORTS[type][report].description,
+              reportLink: `${S3_BASE_URL}${REPORTS[type][report].url}`
+            },
+            // truncate data before attaching
+            data: data[type][report].slice(0, TRUNCATED_COUNT)
+          }
+          shapedData[type].push(report)
+        }
       }
-
-
     }
+
     // build charts
     const metrics = {
       topSearchTermsMetric: buildDescBarChartMetric(data.GLOBAL.TOP_SEARCH_TERMS)
     };
 
     // save to friendly namespace
-    const global = data.GLOBAL
-    const orgs = orgData
+    const global = shapedData.GLOBAL
+    const orgs = shapedData.ORG
 
     return {
       global,
